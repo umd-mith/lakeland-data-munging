66438f63d0503c7c4035bac74655f596051fa44a
Ed Summers <ehs@pobox.com>
2021-01-10 22:34:54 -0500

    separated schema models from migration script

--- migrate_ldt.py
+++ migrate_ldt.py
@@ -1,86 +1,77 @@
 #!/usr/bin/env python3
 
 import os
-import json
-import pathlib
+import re
 
+from schema import Base
 from dotenv import load_dotenv
-from airtable import Airtable
 
 load_dotenv()
 airtable_key = os.environ.get('AIRTABLE_KEY')
 
-data = pathlib.Path('data')
+# Lakeland Digitization Tracking Airtable
 
-def get_base(base_id, schema, force=False):
-    base = {}
-
-    # get the raw tables
-    for table_name in schema.keys():
-        cached_data = data / (table_name + '.json')
-        if cached_data.exists():
-            table = json.load(cached_data.open())
-        else:
-            airtable = Airtable(base_id, table_name, airtable_key)
-            table = airtable.get_all()
-            json.dump(table, cached_data.open('w'), indent=2)
-        base[table_name] = table
-
-    # replace record ids with the relevant objects
-    for table_name, foreign_keys in schema.items():
-        table = base[table_name]
-        for col, foreign_table_name in foreign_keys.items():
-            for row in table:
-                if col not in row['fields']:
-                    continue
-                vals = row['fields'][col]
-                index = get_index(base[foreign_table_name])
-                vals = list(map(index, vals))
-                row['fields'][col] = vals
-
-    return base
-
-def get_index(table):
-    m = {}
-    for rec in table:
-        m[rec['id']] = rec
-
-    def f(id):
-        return m.get(id)
-
-    return f
-
-ldt = get_base("appkzHtR3oryuaKfm", {
+ldt = Base("appkzHtR3oryuaKfm", airtable_key, {
     "Folder": {
-        "Linked Items": "Items",
-        "Linked Images": "Images"
+        "Donor Name": "People"
     },
-    "Items": {
-        "Images in Item": "Images",
-        "People": "People",
-        "Subjects": "Subjects",
-        "Places/Organizations": "Locations"
-    },
-    "Images": {
-        "People (Image Level)": "People",
-        "Subjects (Image Level)": "Subjects",
-        "Places (Image Level)": "Locations"
-    },
+    "Items": {},
+    "Images": {},
     "People": {},
     "Subjects": {},
     "Locations": {},
     "QA": {}
 })
 
-for folder in ldt['Folder']:
-    print(json.dumps(folder, indent=2))
-    break
 
-#l = get_base(
-#    "appqn0kIOXRo00kdN",
-#    key,
-#    []
-#)
+# Lakeland Airtable Airtable
 
+lak = Base('appqn0kIOXRo00kdN', airtable_key, {
+    "Accessions": {},
+    "Files": {},
+    "Items": {},
+    "People": {},
+    "Places": {},
+    "Subjects": {},
+    "Organizations": {}
+})
 
+# start with a clean slate
+lak.wipe()
 
+def parse_name(s):
+    """
+    Parse a name string into its parts.
+    """
+    parts = s.split(' ')
+    f = parts.pop(0)
+    l = parts.pop()
+    s = None
+    if re.match(r'^(sr)|jr|[iv]+$', l, re.IGNORECASE):
+        s = l
+        l = parts.pop()
+    m = ' '.join(parts) if len(parts) > 0 else None
+    return f, m, l, s
+
+# Folders -> Accessions, Files, People
+for f in ldt.tables['Folder'].data:
+    donors = []
+    for person in f['fields']['Donor Name']:
+        first, middle, last, suffix = parse_name(person['fields']['Name'])
+        d = lak.tables['People'].get_or_insert({
+            "First Name": first,
+            "Middle Name": middle,
+            "Last Name": last,
+            "Suffix Name": suffix
+        })
+        donors.append(d['id'])
+
+    lak.tables['Accessions'].insert({
+        "Donor": donors,
+        "Date of Donation": f["fields"].get("Date of donation"),
+        "Description": f["fields"].get("Accession Notes")
+    })
+
+# Images -> Items, Subjects, People, Places
+for f in ldt.tables['Images'].data:
+    pass
---
+++ schema.py
@@ -0,0 +1,115 @@
+import json
+import pathlib
+
+from airtable import Airtable
+
+
+# location of the project and data files
+ROOT_DIR = pathlib.Path(__file__).parent
+DATA_DIR = ROOT_DIR / "data"
+
+
+class Base:
+    """
+    Represents an Airtable Base and all its Tables.
+    """
+
+    def __init__(self, base_id, api_key, schema):
+        self.id = base_id
+        self.tables = {}
+        self.api_key = api_key
+        self.schema = schema
+
+        for table_name, relations in schema.items():
+            self.tables[table_name] = Table(self, table_name, relations)
+
+        for table in self.tables.values():
+            table.link()
+        
+    def wipe(self):
+        """
+        Empty all the tables but leave the schema intact.
+        """
+        for table in self.tables.values():
+            table.wipe()
+
+
+class Table:
+    """
+    Represents an Airbase table.
+    """
+
+    def __init__(self, base, table_name, relations):
+        self.base = base
+        self.table_name = table_name
+        self.relations = relations
+        self.airtable = Airtable(base.id, table_name, base.api_key)
+        self.cache_file = DATA_DIR / (base.id + "-" + table_name + '.json')
+
+        if self.cache_file.exists():
+            self.data = json.load(self.cache_file.open())
+        else:
+            self.data = self.airtable.get_all()
+            json.dump(self.data, self.cache_file.open('w'), indent=2)
+
+    def insert(self, row):
+        result = self.airtable.insert(row)
+        self.data.append(result)
+        self.link()
+        return result
+
+    def get(self, id):
+        for row in self.data:
+            if row['id'] == id:
+                return row
+        return None
+
+    def find(self, fields, first=False):
+        results = []
+        for row in self.data:
+            match = True
+            for k, v in fields.items():
+                if k not in row['fields']:
+                    if v is not None:
+                        match = False
+                elif row['fields'][k] != v:
+                    match = False
+            if match:
+                results.append(row)
+        if first:
+            if len(results) == 0:
+                return None
+            else:
+                return results[0]
+        return results
+
+    def get_or_insert(self, fields):
+        r = self.find(fields, first=True)
+        if not r:
+            r = self.insert(fields)
+        return r
+
+    def link(self):
+        """
+        Use the table's schema relations to turn IDs into objects.
+        """
+        for row in self.data:
+            for prop, other_table_name in self.relations.items():
+                other_table = self.base.tables[other_table_name]
+                if prop in row['fields']:
+                    value = row['fields'][prop]
+                    if type(value) == list:
+                        new_value = []
+                        for v in value:
+                            new_value.append(other_table.get(v))
+                        row['fields'][prop] = new_value
+                    else:
+                        row['fields'][prop] = other_table.get(value)
+
+    def wipe(self):
+        """
+        Remove all rows from the table, and remove any cached data.
+        """
+        for row in self.data:
+            self.airtable.delete(row['id'])
+        self.cache_file.unlink()
